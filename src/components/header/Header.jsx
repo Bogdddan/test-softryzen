01bf260cfbde78f8a6df90d2dd74562d402bdfc1
Bogdan <bogdankrajnik95@gmail.com>
2024-03-22 19:23:53 +0200

    modal window

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
 import css from './header.module.css';
 import logo from '../../svg/logo.svg';
+import modalLogo from '../../svg/modal-logo.svg';
 import logoShip from '../../svg/logo-ship.svg';
 import xLogo from '../../svg/xLogo.svg';
 import discordLofo from '../../svg/discord_logo.svg';
+import modalDiscord from '../../svg/discord-modal.svg';
+import xModal from '../../svg/x-modal.svg';
+import shipModal from '../../svg/ship-modal.svg';
 
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = (e) => {
+    e.preventDefault();
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <>
       <ul className={css.headerList}>
@@ -21,15 +32,34 @@ function Header() {
         </li>
         <li>
           <ul className={css.nav}>
-            <li><div className={css.menuSchema}>Menu</div></li>
+            <li><button onClick={toggleModal} className={css.menuSchema}>Open</button></li>
             <li><div className={css.menuSchema}><img src={discordLofo} alt="disLogo" /></div></li>
             <li><div className={css.menuSchema}><img src={logoShip} alt="shipLogo" /></div></li>
             <li><div className={css.menuSchema}><img className={css.xlogo} src={xLogo} alt="logoX" /></div></li>
           </ul>
         </li>
       </ul>
+      {isModalOpen && (
+        <div className={css.modal}>
+          <img src={modalLogo} alt="logo" />
+          <ul className={css.modalNav}>
+            <li>ABOUT</li>
+            <li>MIND-MAP</li>
+            <li>FAQ</li>
+            <li>ARTS</li>
+            <li>MINT</li>
+            <li><p className={css.revised}>© Yacht ape 2024 all rights <br /> reserved</p></li>
+          </ul>
+          <ul className={css.nav}>
+            <li><button onClick={toggleModal} className={css.menuSchemsModal}>Close</button></li>
+            <li><div className={css.menuSchemsModal}><img src={modalDiscord} alt="disLogo" /></div></li>
+            <li><div className={css.menuSchemsModal}><img src={shipModal} alt="shipLogo" /></div></li>
+            <li><div className={css.menuSchemsModal}><img className={css.xlogo} src={xModal} alt="logoX" /></div></li>
+          </ul>
+        </div>
+      )}
     </>
-  )
-};
+  );
+}
 
 export default Header;
