d6816778e34d74825af6aa45cfdc249302a16b2c
Bogdan <bogdankrajnik95@gmail.com>
2024-03-24 01:58:12 +0200

    contactUs

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -1,7 +1,20 @@
+import css from './contactUs.module.css';
+
 function ContactUs() {
-    return (
-      <div>ContactUs</div>
-    )
-  };
-  
-  export default ContactUs;
+  return (
+    <>
+      <h2>Are you in?</h2>
+      <ul className={css.contactUsList}>
+        <li><div>x</div></li>
+        <li><p>JOIN THE YACHT APE COMMUNITY TO BE ONE OF THE FIRST TO RECIVE OUR LIMITED EDITION NFT</p></li>
+        <li><form className={css.contactUsForm}>
+          <input type="text" placeholder='@USERNAME' />
+          <input type="text" placeholder='WALLET ADDRESS' />
+          <button className={css.contactUsFormButton}><p>Mint</p></button>
+        </form></li>
+      </ul>
+    </>
+  )
+};
+
+export default ContactUs;
