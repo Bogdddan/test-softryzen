da4d7dc9eb3ea2fafa99deba66817f2bece4cafa
Bogdan <bogdankrajnik95@gmail.com>
2024-03-25 01:21:00 +0200

    header tabl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 // import css from 'index.css';
 import About from './about/About';
-import Arts from './arts/Arts';
+// import Arts from './arts/Arts';
 // import Burger from './burger/Burger';
 import ContactUs from './contactUs/ContactUs';
 import FAQ from './faq/Faq';
 import Footer from './footer/Footer';
 import Header from './header/Header';
 // import Hero from './hero/Hero';
-import MindMap from './mindMap/MindMap';
+// import MindMap from './mindMap/MindMap';
 import css from './App.module.css';
 
 
@@ -16,8 +16,8 @@ export const App = () => {
     <div className={css.container}>
       <Header/>
       <About/>
-      <MindMap/>
-      <Arts/>
+      {/* <MindMap/> */}
+      {/* <Arts/> */}
       <FAQ/>
       <ContactUs/>
       <Footer/>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -27,21 +27,44 @@ function Header() {
             <p className={css.text}>Apes aRe eveRywhere</p>
           </div>
           <img className={css.photo} src="https://s3-alpha-sig.figma.com/img/e878/96ec/ecc8928dd98e38ca2991c1f90127dd7c?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ku7oLN6GgCaHqqCAkzX8WWJRcYiPKrrDtseCfyBX3kdq~eJKGuB0qljtUHAhPcRJbWyEVUjU5~-A-Oyw--FJnDxYhTOrUiMpsC39ivAKxIc5GwVQZWctA1wB7-TovF9sB5xxe7vYU70ifLwtmcg8eauEjvOTUrMwp1FqnpJkJTZNfq0U6kCqOoFzDlqH2fhLVhUzu3CcWKxuU--CUND1gqUAFK4jK0p564rRt80rwcB7Hl63r4q49sGKXG8-WdTgj35CjMZYRG9~mJ973EuSEVx-slESZ7T6xGuGUinyUsywT0-lEQXC~qL2J~NY9mJpN274dZMcLcTkYCEGTM58tA__" alt="ape" /><br />
-          <button className={css.headerButton}><p>Meet Apes</p></button>
-          <p className={css.apeDescription}>Yacht Ape is a collection of <br /> unique digital apes that you <br /> can own in NFT format</p>
+          <div className={css.buttonText}>
+            <button className={css.headerButton}><p>Meet Apes</p></button>
+            <p className={css.apeDescription}>Yacht Ape is a collection of <br /> unique digital apes that you <br /> can own in NFT format</p>
+          </div>
         </li>
         <li>
           <ul className={css.nav}>
-            <li><button onClick={toggleModal} className={css.menuSchema}>Open</button></li>
-            <li><div className={css.menuSchema}><img src={discordLofo} alt="disLogo" /></div></li>
-            <li><div className={css.menuSchema}><img src={logoShip} alt="shipLogo" /></div></li>
-            <li><div className={css.menuSchema}><img className={css.xlogo} src={xLogo} alt="logoX" /></div></li>
+            <li>
+              <div className={css.tablMenu}>
+                <button onClick={toggleModal} className={css.menuSchema}>{isModalOpen ? 'CLOSE' : 'OPEN'}</button>
+                {isModalOpen && (
+
+                  <ul className={css.modalNavTabl}>
+                    <li>ABOUT</li>
+                    <li>MIND-MAP</li>
+                    <li>FAQ</li>
+                    <li>ARTS</li>
+                    <li>MINT</li>
+                  </ul>
+
+                )}
+              </div>
+            </li>
+            <li>
+              <div className={css.menuSchema}><img src={discordLofo} alt="disLogo" /></div>
+            </li>
+            <li>
+              <div className={css.menuSchema}><img src={logoShip} alt="shipLogo" /></div>
+            </li>
+            <li>
+              <div className={css.menuSchema}><img className={css.xlogo} src={xLogo} alt="logoX" /></div>
+            </li>
           </ul>
         </li>
       </ul>
       {isModalOpen && (
         <div className={css.modal}>
-          <img src={modalLogo} alt="logo" />
+          <img className={css.modalLogo} src={modalLogo} alt="logo" />
           <ul className={css.modalNav}>
             <li>ABOUT</li>
             <li>MIND-MAP</li>
@@ -50,7 +73,7 @@ function Header() {
             <li>MINT</li>
             <li><p className={css.revised}>© Yacht ape 2024 all rights <br /> reserved</p></li>
           </ul>
-          <ul className={css.nav}>
+          <ul className={css.navNav}>
             <li><button onClick={toggleModal} className={css.menuSchemsModal}>Close</button></li>
             <li><div className={css.menuSchemsModal}><img src={modalDiscord} alt="disLogo" /></div></li>
             <li><div className={css.menuSchemsModal}><img src={shipModal} alt="shipLogo" /></div></li>
