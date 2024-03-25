b67716536d7f333926f975869fb84b7a662bdcd5
Bogdan <bogdankrajnik95@gmail.com>
2024-03-25 18:16:37 +0200

    mindMap tabl desk

--- src/components/mindMap/MindMap.jsx
+++ src/components/mindMap/MindMap.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import css from './mindMap.module.css';
+import arrow from '../../svg/arrow.svg';
 
 function MindMap() {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const cards = [
     <div className={css.card}>
-      <p>All owners of APE NFTs and all future collections will receive a percentage of sales based on the number of NFTs they own</p>
-      <p>Yape Drop</p>
+      <p className={css.mindMapDesc}>All owners of APE NFTs and all future collections will receive a percentage of sales based on the number of NFTs they own</p>
+      <p className={css.mindMapNav}>Yape Drop</p>
     </div>,
     <div className={css.card}>
-      <p>Launch of the 2nd YACHT Collection Releasing the first version of the Ape Slam Game</p>
-      <p>New Collection</p>
+      <p className={css.mindMapDesc}>Launch of the 2nd YACHT Collection Releasing the first version of the Ape Slam Game</p>
+      <p className={css.mindMapNav}>New Collection</p>
     </div>,
     <div className={css.card}>
-      <p>Launch your own token, the proceeds of which will go to a global fund to LAUNCH YACHT CLUB AND PROMOTE it</p>
-      <p>Token</p>
+      <p className={css.mindMapDesc}>Launch your own token, the proceeds of which will go to a global fund to LAUNCH YACHT CLUB AND PROMOTE it</p>
+      <p className={css.mindMapNav}>Token</p>
     </div>,
     <div className={css.card}>
-      <button>Learn morein mind map</button>
+      <img src={arrow} alt="arrow" />
+      <p className={css.mindMapNav}>LEARN MORE IN MIND MAP</p>
     </div>,
   ];
 
@@ -37,6 +39,13 @@ function MindMap() {
         {cards.map((card, index) => (
           <li key={index} style={{ display: index === currentSlide ? 'block' : 'none' }}>
             <div className={css.mindMapItem}> {card} </div>
+          </li>
+        ))}
+      </ul>
+      <ul className={css.mindMapListTablDesk}>
+        {cards.map((card, index) => (
+          <li key={index}>
+            <div> {card} </div>
           </li>
         ))}
       </ul>
